644ea65de504a342c7f3b27257f2c18d74bb1902
ngiy <y.nag.bas@gmail.com>
2017-05-27 22:15:21 +0900

    github

--- coffeetech.js
+++ coffeetech.js
@@ -1,6 +1,6 @@
 var mod = angular.module( 'coffeetech', [] );
 mod.controller( 'GithubCtrl', function( $scope ) {
-  var github = new Github({} );
+  var github = new GitHub({} );
   var repo = github.getRepo( "gollum", "gollum" );
   repo.show( function(err, repo) {
     $scope.repo = repo;
